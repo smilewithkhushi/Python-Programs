46d3bd9fe044ab664b07602b72f12feeab123ef7
Khushi Panwar <102166679+smilewithkhushi@users.noreply.github.com>
2022-05-10 19:11:14 +0530

    GeeksForGeeks Question

--- kth smallest element from list.py
+++ kth smallest element from list.py
@@ -1,4 +1,18 @@
-list=[5,83,23,45,23,54,12,2]
+'''
+Given an array arr[] and an integer K where K is smaller than size of array,
+the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+Output : 7
+Explanation :
+3rd smallest element in the given 
+array is 7.
+'''
+
+
+list=[7,10 ,4, 3,20,15]
 
 k=int(input("Enter the value of k :"))
 
